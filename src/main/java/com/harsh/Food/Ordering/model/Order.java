05abbdae8baed8f9dde683228c7110a13757e5ae
Harsh Nagar <harshnagar3006@gmail.com>
2024-08-09 02:47:18 +0530

    order.java added

---
+++ src/main/java/com/harsh/Food/Ordering/model/Order.java
@@ -0,0 +1,43 @@
+package com.harsh.Food.Ordering.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne
+    private User customer;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private Long totalAmount;
+
+    private String orderStatus;
+
+    private Date createdAt;
+
+    private Address deliveryAddress;
+
+    private List<OrderItem> items;
+
+    // private Payment payment
+    private int totalItem;
+
+    private int totalPrice;
+}
