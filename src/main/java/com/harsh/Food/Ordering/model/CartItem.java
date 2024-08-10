e48f2463d26b9403ecf2d0ec043c0e2e82d75d75
Harsh Nagar <harshnagar3006@gmail.com>
2024-08-10 16:45:19 +0530

    cart item is added

---
+++ src/main/java/com/harsh/Food/Ordering/model/CartItem.java
@@ -0,0 +1,33 @@
+package com.harsh.Food.Ordering.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne
+    @JsonIgnore
+    private Cart cart;
+
+    @ManyToOne
+    private Food food;
+
+    private int quantity;
+
+    private List<String> ingredients;
+
+    private Long totalPrice;
+
+}
