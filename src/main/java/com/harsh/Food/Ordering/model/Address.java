cf95bad6ef84fd96bdd44a41bf6a277a01318ee4
Harsh Nagar <harshnagar3006@gmail.com>
2024-08-10 16:39:55 +0530

    changes made to this file

--- src/main/java/com/harsh/Food/Ordering/model/Address.java
+++ src/main/java/com/harsh/Food/Ordering/model/Address.java
@@ -1,13 +1,21 @@
 package com.harsh.Food.Ordering.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    private String city; // made this at address error
+    private String district;// made this at address error
+
+    private String state;// made this at address error
+
+
+
+
 }
