0fba98cb4a7a2b4eccbfb5e29037a94b60f8c548
Harsh Nagar <harshnagar3006@gmail.com>
2024-08-12 19:09:45 +0530

    updated

---
+++ src/main/java/com/harsh/Food/Ordering/model/ContactInformation.java
@@ -0,0 +1,16 @@
+package com.harsh.Food.Ordering.model;
+
+import lombok.Data;
+
+@Data
+public class ContactInformation {
+
+    private String email;
+
+    private String mobile;
+
+    private String twitter;
+
+    private String instagram;
+
+}
